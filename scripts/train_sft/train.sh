c01d9dfb97a551982892a4c761250c63adfe8ae1
Sayan Deb Sarkar <sayandsarkar.1997@gmail.com>
2024-06-11 03:01:05 +0200

    Fixed typo

--- scripts/train_sft/train.sh
+++ scripts/train_sft/train.sh
@@ -3,4 +3,4 @@ export WANDB_PROJECT="noisy-lms"
 export WANDB_LOG_MODEL="checkpoint"
 
 cd $NOISY_LM_DIR
-python src/trainers/sft_trainer.py --config $NOISY_LM_DIR/scripts/train_sft/train_sft.yaml
+python src/trainers/supervised_finetune_trainer.py --config $NOISY_LM_DIR/scripts/train_sft/train_sft.yaml
