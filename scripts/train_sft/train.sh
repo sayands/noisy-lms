af62ba98e4b032381ebb5af47740df8ad98b1ce8
Sayan Deb Sarkar <sayandsarkar.1997@gmail.com>
2024-06-11 02:46:56 +0200

    Added usage scripts

---
+++ scripts/train_sft/train.sh
@@ -0,0 +1,6 @@
+export NOISY_LM_DIR=$(pwd)
+export WANDB_PROJECT="noisy-lms"
+export WANDB_LOG_MODEL="checkpoint"
+
+cd $NOISY_LM_DIR
+python src/trainers/sft_trainer.py --config $NOISY_LM_DIR/scripts/train_sft/train_sft.yaml
