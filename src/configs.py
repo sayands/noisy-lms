be845ee815d1f795a7f96cdbfd40ffa13d4a2f37
Sayan Deb Sarkar <sayandsarkar.1997@gmail.com>
2024-06-11 03:50:01 +0200

    Fixed circular dependency

--- src/configs.py
+++ src/configs.py
@@ -2,24 +2,6 @@ from dataclasses import dataclass
 from typing import Any, Protocol, Union
 import datasets as common
 
-# from src.datasets.openai_humanfeedback import OpenAIHumanFeedbackDatasetPreprocessor
-
-# DATASET_NAME_TO_PREPROCESSOR = {
-#     "openai/summarize_from_feedback": OpenAIHumanFeedbackDatasetPreprocessor,
-# }
-
-@dataclass
-class DatasetConfig:
-    dataset_name: str
-    dataset_noise_type: str = "flip_labels"
-    dataset_noise_level: float = 0.0
-    dataset_noise_seed: int = 42
-    preprocess_for_reward_trainer: bool = False
-    preprocess_tokenizer: Union[str, None] = None
-
-    # def make_dataset(self) -> common.DatasetDict:
-    #     return DATASET_NAME_TO_PREPROCESSOR[self.dataset_name].from_config(self)
-
 @dataclass
 class TrainingConfig:
     dataset_name: str
